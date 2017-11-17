dd0433f9cd5d878e2f08ba4bae952ba6be496153
Christopher Eliasson <ChristopherEliasson@gmail.com>
2017-11-17 23:32:31 +0100

    - Updated constraints for AssetTableViewCell

--- Cryptfolio/Views/TableViewCells/AssetTableViewCell.swift
+++ Cryptfolio/Views/TableViewCells/AssetTableViewCell.swift
@@ -16,38 +16,71 @@ class AssetTableViewCell: UITableViewCell {
     var iconImageView: UIImageView!
     var tickerLabel: UILabel!
     var fullnameLabel: UILabel!
+    var containerView: UIView!
+    var didSetupConstraints = false
 
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        // Add subviews
+        self.containerView = UIView()
+        self.containerView.translatesAutoresizingMaskIntoConstraints = false
+        self.containerView.clipsToBounds = true
+        self.addSubview(self.containerView)
+        
         self.iconImageView = UIImageView()
+        self.iconImageView.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(self.iconImageView)
         
-        self.iconImageView.autoPinEdge(toSuperviewMargin: .left)
-        self.iconImageView.autoPinEdge(toSuperviewMargin: .top)
-        self.iconImageView.autoSetDimensions(to: CGSize(width: 30.0, height: 30.0))
-        
         self.tickerLabel = UILabel()
-        self.addSubview(self.tickerLabel)
+        self.tickerLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.tickerLabel.textAlignment = .left
+        self.containerView.addSubview(self.tickerLabel)
         
-        self.tickerLabel.autoPinEdge(toSuperviewMargin: .bottom)
-        self.tickerLabel.autoPinEdge(.left, to: .left, of: self, withOffset: 8)
-        self.tickerLabel.autoPinEdge(.top, to: .bottom, of: self.iconImageView, withOffset: 2.0)
-        self.tickerLabel.autoAlignAxis(.horizontal, toSameAxisOf: self.iconImageView)
-        self.tickerLabel.autoSetDimension(.width, toSize: 40.0)
-        self.tickerLabel.autoSetDimension(.height, toSize: 14.5)
+        self.fullnameLabel = UILabel()
+        self.fullnameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.containerView.addSubview(self.fullnameLabel)
         
+        self.setNeedsUpdateConstraints()
+    }
+    
+    override func updateConstraints() {
         
+        if (!self.didSetupConstraints) {
+            self.iconImageView.autoSetDimensions(to: CGSize(width: 34.0, height: 34.0))
+            self.iconImageView.autoPinEdge(toSuperviewEdge: .left, withInset: 16.0)
+            self.iconImageView.autoAlignAxis(.horizontal, toSameAxisOf: self)
+            
+            self.containerView.autoAlignAxis(.horizontal, toSameAxisOf: self)
+            self.containerView.autoPinEdge(toSuperviewMargin: .top)
+            self.containerView.autoPinEdge(toSuperviewMargin: .bottom)
+            self.containerView.autoPinEdge(.left, to: .right, of: self.iconImageView, withOffset: 8.0)
+            self.containerView.autoPinEdge(toSuperviewMargin: .right)
+    
+            self.fullnameLabel.autoPinEdge(toSuperviewEdge: .top)
+            self.fullnameLabel.autoPinEdge(toSuperviewMargin: .right)
+            self.fullnameLabel.autoPinEdge(toSuperviewEdge: .left)
+            self.fullnameLabel.autoPinEdge(.bottom, to: .top, of: self.tickerLabel)
+            
+            self.tickerLabel.autoPinEdge(toSuperviewEdge: .right)
+            self.tickerLabel.autoPinEdge(toSuperviewEdge: .left)
+            self.tickerLabel.autoPinEdge(toSuperviewEdge: .bottom)
+            self.tickerLabel.autoMatch(.width, to: .width, of: self.fullnameLabel)
+            
+            self.didSetupConstraints = true
+        }
+        super.updateConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        DDLogDebug("Init(coder:) has not been implemented")
+        DDLogError("Init(coder:) has not been implemented")
         fatalError("init(coder:) has not been implemented")
     }
     
     private func style() {
-        // style
+        self.tickerLabel.font = UIFont(name: self.tickerLabel.font.fontName, size: 12.0)
+        self.fullnameLabel.font = UIFont(name: self.fullnameLabel.font.fontName, size: 14.0)
     }
     
     func setup(coin: Coin) {
@@ -58,6 +91,6 @@ class AssetTableViewCell: UITableViewCell {
         }
         
         self.tickerLabel.text = coin.ticker
-        //self.fullnameLabel.text = coin.fullname
+        self.fullnameLabel.text = coin.fullname
     }
 }
